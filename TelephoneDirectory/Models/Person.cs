a1e24b38669015ecea3293c7c03b4a1e26f6b38e
SüleymanYılmaz <suleymanyilmaz95@gmail.com>
2021-10-19 11:34:39 +0300

    Kişi modeli oluşturuldu.

---
+++ TelephoneDirectory/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TelephoneDirectory.Models
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public Guid Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Surname")]
+        public string Surname { get; set; }
+
+        [BsonElement("Company")]
+        public string Company { get; set; }
+
+        public ICollection<Contact> Contact { get; set; }
+    }
+}
